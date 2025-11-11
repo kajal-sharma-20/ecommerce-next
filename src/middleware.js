1a48e514f512ebf4aff4b3e97a6561538854bac9
kajal-sharma-20 <kajal0120sharma@gmail.com>
2025-11-12 00:04:22 +0530

    changes

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,19 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  // Get token from cookies
   const token = req.cookies.get("token")?.value;
 
-  // If no token, redirect to login page
-  if (!token) {
+  // Allow /admin/login without token
+  if (req.nextUrl.pathname.startsWith("/admin/login")) {
+    return NextResponse.next();
+  }
+
+  // Protect other /admin routes
+  if (req.nextUrl.pathname.startsWith("/admin") && !token) {
     const url = req.nextUrl.clone();
-    url.pathname = "/"; // your login page
+    url.pathname = "/admin/login"; // redirect to login
     return NextResponse.redirect(url);
   }
 
-  // If token exists, continue
   return NextResponse.next();
 }
-
-// Protect all /admin routes
-export const config = {
-  matcher: ["/admin/:path*"],
-};
